d2e9d97ea34ca8d182eb1ca0e0a73158da7eab83
Oleg Pustovalov <oleg@portalnews.ru>
2018-12-11 12:11:57 +0300

    Создана миграция для создания таблицы auth

---
+++ console/migrations/m181211_090941_create_table_auth.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m181211_090941_create_table_auth
+ */
+class m181211_090941_create_table_auth extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('auth', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'source' => $this->string()->notNull(),
+            'source_id' => $this->string()->notNull(),
+        ]);
+
+        $this->addForeignKey('fk-auth-user_id-user-id', 'auth', 'user_id', 'user', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('auth');
+    }
+}
